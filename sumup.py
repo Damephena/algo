5a6a35a26a16a41bb994b892970806c2d59bcf58
damephena <okoyeifenna24@gmail.com>
2020-06-17 23:17:29 +0100

    format fix

--- sumup.py
+++ sumup.py
@@ -2,39 +2,36 @@
 A program that returns any two numbers from a list which adds up to a target.
 
 @params arr: a list
-@params k: an integer
+@params target_value: an integer
 @returns: a boolean. True returns a tuple with result.
 '''
 from itertools import permutations
-my_list = [10, 15, 3, 7]
-k = 17
 
-# using permutations to go through the list only once.
-def find_sum(arr, k):
-    result = [numbers for numbers in permutations(arr, 2) if sum(numbers) == k]
+def find_sum(arr, target_value):
+    '''Using permutations. Loops through the list only once'''
+    result = [numbers for numbers in permutations(arr, 2) if sum(numbers) == target_value]
     return False if not result else (True, result)
 
 # using range
-def find_sum(arr, k):
+def find_sum2(arr, target_value):
+    '''Using range. Loops through list twice. Returns unique value if True.'''
     result = []
     for first_number in arr:
         for i in range(len(arr) - 1):
-            if first_number + arr[i + 1] == k:
+            if first_number + arr[i + 1] == target_value:
                 result.append((first_number, arr[i + 1]))
-
-    return False if not result else True
+    return False if not result else (True, result)
 
 # using enumerate
-def find_sum(arr, k):
+def find_sum3(arr, target_value):
+    '''Using enumerate. Loops through list twice.'''
     result = []
     for first_number in arr:
-        for i, last_number in enumerate(arr, 1):
-            if first_number + last_number == k:
+        for _, last_number in enumerate(arr, 1):
+            if first_number + last_number == target_value:
                 result.append((first_number, last_number))
-
     if not result:
         return False
-    else:
-        return True, result
+    return True, result
 
-print(find_sum(my_list, k))
+print(find_sum([10, 15, 3, 7], 17))
