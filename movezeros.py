22c523e6caedc94483b3a0df5e6ebdec0d56d4e2
damephena <okoyeifenna24@gmail.com>
2020-06-18 21:17:17 +0100

    in-place solution

--- movezeros.py
+++ movezeros.py
@@ -9,11 +9,20 @@ Maintains the relative order of non-zero elements.
 from collections import Counter
 
 def move_zeroes(arr):
-    '''Returns an array with all zeros at the list's end.'''
+    '''Returns a new array with all zeros at the list's end.'''
     result = [number for number in arr if number != 0]
     for _ in range(Counter(arr)[0]):
         result.append(0)
     return result
+
+def zero(arr):
+    '''Returns same array using in-place swap.'''
+    count = 0
+    for i, value in enumerate(arr):
+        if value != 0:
+            arr[i], arr[count] = arr[count], value
+            count += 1
+    return arr
 
 # returns [1, 3, 12, 0, 0]
 MY_LIST = [0, 1, 0, 3, 12]
@@ -21,5 +30,6 @@ MY_LIST = [0, 1, 0, 3, 12]
 # returns [2, 1, 3, 4, 0, 0, 0, 0, 0, 0]
 ARR = [0, 0, 0, 2, 0, 1, 3, 4, 0, 0]
 
+print(zero(ARR))
+print(MY_LIST)
 print(move_zeroes(ARR))
-print(move_zeroes(MY_LIST))
